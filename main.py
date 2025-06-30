f3d7368eec575d78f1bdbf523226ccd6f4b79b3d
Kanishk Prasad <kanishkprasad6@gmail.com>
2025-06-30 23:32:44 +0530

    new files added

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from routers import users, tasks, admin
 from database import engine
 from models import Base
 from middleware.log_middleware import LoggerMiddleware
+from middleware.rate_limiter import RedisRateLimiter
 from middleware.log_events import lifespan
+from middleware.cors import add_cors_middleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(lifespan=lifespan)
+
+add_cors_middleware(app)
 
 app.include_router(users.router)
 app.include_router(tasks.router)
 app.include_router(admin.router)
 
 app.add_middleware(LoggerMiddleware)
+app.add_middleware(RedisRateLimiter)
---
+++ middleware/rate_limiter.py
@@ -0,0 +1,43 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from jose import JWTError, jwt
+from config import SECRET_KEY, ALGORITHM
+from starlette.middleware.base import BaseHTTPMiddleware
+import redis
+
+redis_client = redis.Redis(host='localhost', port='6379', decode_responses=True)
+
+class RedisRateLimiter(BaseHTTPMiddleware):
+    def __init__(self, app, limit=5, window=60):
+        super().__init__(app)
+        self.limit = limit
+        self.window = window
+
+    async def dispatch(self, request: Request, call_next):
+        auth_header = request.headers.get('Authorization')
+        user_id = 'unknown'
+
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            user_id = payload.get('sub', 'unknown')
+        except JWTError:
+            user_id = 'Invalid Token!'
+
+        redis_key = f'ratelimiting: {user_id}'
+        request_count = redis_client.get(redis_key)
+
+        if request_count is None:
+            redis_client.set(redis_key, 1, ex=self.window)
+        elif request_count < self.limit:
+            redis_client.incr(redis_key)
+        else:
+            ttl = redis_client.ttl(redis_key)
+            return JSONResponse(
+                status_code=429,
+                content = {'message': f'Rate limit exceeded. Try again after {ttl} seconds.'}
+            )
+        
+        return await call_next(request)
